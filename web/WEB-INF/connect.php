42c28bb1b499c972a8b94798067d7775a7be7641
Андрей <5_108@mail.ru>
2019-05-21 16:29:13 +0700

    Prepare to HW0
    Signed-off-by: Андрей <5_108@mail.ru>

--- web/WEB-INF/connect.php
+++ web/WEB-INF/connect.php
@@ -8,3 +8,22 @@ $DB_NAME="tutorial";
 $DB_PORT="8080"; /// Порт базы
 $con = mysqli_connect($DB_SERVER, $DB_USER_READER, $DB_PASS_READER, $DB_NAME, $DB_PORT);
 mysqli_set_charset($con, 'utf8');
+
+$con = mysqli_init();
+if (!$con) {
+    die('mysqli_init завершилась провалом');
+}
+
+if (!mysqli_options($con, MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0')) {
+    die('Установка MYSQLI_INIT_COMMAND завершилась провалом');
+}
+
+if (!mysqli_options($con, MYSQLI_OPT_CONNECT_TIMEOUT, 5)) {
+    die('Установка MYSQLI_OPT_CONNECT_TIMEOUT завершилась провалом');
+}
+
+if (!mysqli_real_connect($con, $DB_SERVER, $DB_USER_READER, $DB_PASS_READER, $DB_NAME, $DB_PORT)) {
+    die('Ошибка подключения (' . mysqli_connect_errno() . ') '
+        . mysqli_connect_error());
+}
+?>
--- web/WEB-INF/rec.php
+++ web/WEB-INF/rec.php
@@ -1,10 +1,10 @@
 <?php
-require 'connect.php';
+include 'connect.php';
 $Order_number = trim($_REQUEST['Order_number']);
 $insert_sql = "INSERT INTO tutorial.order_number (Order_number)" . // Указывает в какую таблицу и в какие поля ...
                    "VALUES('{$Order_number}');"; // ...записывать данные
 
-mysqli_query($insert_sql); // отправляем данные в базу
+mysqli_query($link, $insert_sql); // отправляем данные в базу
 
 ?>
 <html lang="ru">
