e98e0bd6158ea6c1fd4334f6d0cce4fdb9b362ab
Андрей <5_108@mail.ru>
2019-05-22 12:31:34 +0700

    Prepare to HW0
    Signed-off-by: Андрей <5_108@mail.ru>

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,23 +1,37 @@
+import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 
 public class JDBC {
-    private static final String url = "jdbc:mysql://localhost:3306/?useLegacyDatetimeCode=false&amp&serverTimezone=UTC&useSSL=false";
+    private static final String url = "jdbc:mysql://localhost:3306/tutorial?useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
     private static final String username = "root";
     private static final String password = "Element11+";
 
     public static void main(String[] args) {
-       Connection connection;
+
         try {
-
-            connection = DriverManager.getConnection(url, username, password);
-
-            if (!connection.isClosed()) {
-
-                System.out.println("Соединение с БД установлено");
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Соединение успешно!");
+            Connection connection= DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from tutorial.order_number");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(2));
             }
-        }catch (SQLException e) {
+resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e){
             e.printStackTrace();
-            System.out.println("Не удалось загрузить класс драйвера");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
     }
     }
